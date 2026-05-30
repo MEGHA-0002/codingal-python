f0acb007929197d0e8a3d864f7e19dcd14ed8925
Megha Ajith <meghaajith002@gmail.com>
2026-05-30 21:59:25 +0530

    thirdd

---
+++ flow.py
@@ -0,0 +1,13 @@
+# 1. Secret Number Guessing Game 🎲
+
+# Task: Write the algorithm first, then draw a flowchart.
+
+# Algorithm
+
+# Start
+# Store a secret number (e.g., 7)
+# Ask the user to guess the number
+# Is the guess equal to the secret number?
+# Yes → Print "Correct!"
+# No → Print "Try Again"
+# Stop
