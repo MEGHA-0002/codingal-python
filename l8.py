8e531945730674442b53290bf02aa180a1dc7231
MEGHA AJITH <meghaajith002@gmail.com>
2026-05-30 11:36:57 +0530

    Refactor operator precedence and divisibility checks

--- l8.py
+++ l8.py
@@ -1,71 +1,65 @@
-#operator precedance
+# ==========================================
+# Lesson 8 - Operator Precedence and Applications
+# ==========================================
+
+# ==========================================
+# Activity 1: Operator Precedence
+# ==========================================
+
 v = 4
 w = 5
 x = 8
 y = 2
-z = 0
-z = (v+w) * x / y;   
-print("Value of (v+w) * x/ y is ",  z)
 
+result = (v + w) * x / y
 
+print("Value of (v + w) * x / y is", result)
 
 name = "Alex"
 age = 0
-  
-if name == "Alex" or name == "John" and age >= 2 : 
-  print("Hello! Welcome.")
-else :
-  print("Good Bye!!")
 
-  #divivsible no
-
-  print("Enter a Number (Numerator): ")
-numn = int(input())
-print("Enter a Number (denominator): ")
-numd = int(input())
-
-if numn%numd==0:
-  print("\n" +str(numn)+ " is divisible by " +str(numd))
+if name == "Alex" or name == "John" and age >= 2:
+    print("Hello! Welcome.")
 else:
-  print("\n" +str(numn)+ " is not divisible by " +str(numd))
+    print("Good Bye!!")
 
-  ##mean value
-  mean1 = 38
-wrong_number=36
-correct_number=56
-total_number=40
-#sum of 40 numbers
-sum = mean1*total_number
-print("the sum of 40 number: ",sum)
 
-#correct sum of these numbers
-num2=sum-((wrong_number)-(correct_number))
-print("sum-((wrong_number)-(correct_number)): ",num2)
+# ==========================================
+# Activity 2: Divisibility Checker
+# ==========================================
 
-#the correct mean
-mean2=num2/total_number
-print(mean2)
-##avg speeed
-a = int(input("enter a value: "))
-b = int(input("enter value 2 :"))
-c = int(input("enter value 3: "))
+print("\nEnter a Number (Numerator):")
+numerator = int(input())
 
-avg = (a + b + c) / 3
-print("avg =", avg)
+print("Enter a Number (Denominator):")
+denominator = int(input())
 
-if avg > a and avg > b and avg > c:
-    print("%d is higher than %d, %d, %d" %(avg, a, b, c))
-elif avg > a and avg > b:
-    print("%d is higher than %d, %d" %(avg, a, b))
-elif avg > a and avg > c:
-    print("%d is higher than %d, %d" %(avg, a, c))
-elif avg > b and avg > c:
-    print("%d is higher than %d, %d" %(avg, b, c))
-elif avg > a:
-    print("%d is just higher than %d" %(avg, a))
-elif avg > b:
-    print("%d is just higher than %d" %(avg, b))
-elif avg > c:
-    print("%d is just higher than %d" %(avg, c))
+if numerator % denominator == 0:
+    print(str(numerator) + " is divisible by " + str(denominator))
 else:
-  print("invalid input")
+    print(str(numerator) + " is not divisible by " + str(denominator))
+
+
+# ==========================================
+# Activity 3: Mean Value Calculator
+# ==========================================
+
+mean1 = 38
+wrong_number = 36
+correct_number = 56
+total_numbers = 40
+
+# Sum based on incorrect mean
+
+total_sum = mean1 * total_numbers
+print("The sum of 40 numbers:", total_sum)
+
+# Corrected sum
+
+correct_sum = total_sum - wrong_number + correct_number
+print("Corrected sum:", correct_sum)
+
+# Correct mean
+
+mean2 = correct_sum / total_numbers
+print("Correct Mean =", mean2)
